73391184693637ce2ee6f0dad09c0cbc5bd3d609
Martin Echavez Meza <martinechavez1994@gmail.com>
2021-10-26 17:28:52 -0500

    Cambios en las variables de entorno URLBD

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL;
+const URL = process.env.URL_MONGODB;
 
 // Connection file
 const db = require('./db');
